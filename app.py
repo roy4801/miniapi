55d7befb3f2b89fbd24b47629f03a7fd3b4f05cc
roy4801 <a82611141@gmail.com>
2019-10-28 16:52:48 +0800

    Add Anime class

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from flask_restful import Api, Resource
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,11 +19,39 @@ def make_api_response(results, status=True, message=[], errors=[]):
 	response = jsonify(make_api_dict(results, status, message, errors))
 	return response
 
-class HelloWorld(Resource):
-	def get(self):
-		return make_api_dict([], False), 200
+anime = {
+	1:
+	{
+		"name": "刀劍第三季",
+		"time": "19/秋"
+	},
+	2:
+	{
+		"name": "食戟之靈 （第四季）",
+		"tile": "19/秋"
+	}
+}
 
-api.add_resource(HelloWorld, '/')
+def abort_if_anime_not_found(anime_id):
+	if anime_id not in anime:
+		abort(404, **make_api_dict([], False, [], ['Not Found']))
+
+class AnimeList(Resource):
+	def get(self):
+		return make_api_dict(anime), 200
+
+	def post(self):
+		data = request.get_json()
+		li = None
+		return make_api_dict(li), 200
+
+class Anime(Resource):
+	def get(self, anime_id):
+		abort_if_anime_not_found(anime_id)
+		return make_api_dict(anime[anime_id]), 200
+
+api.add_resource(AnimeList, '/animes')
+api.add_resource(Anime, '/animes/<int:anime_id>')
 
 if __name__ == '__main__':
 	app.run(debug=True)
